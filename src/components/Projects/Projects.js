1f3e74bb5179bb86149b61c6a404704b72f844cd
iamRishabh07 <iamrishabh799@gmail.com>
2024-07-24 18:10:39 +0530

    first commit

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import ProjectCard from './ProjectCards';
+import Particle from '../Particle';
+import leaf from '../../Assets/Projects/leaf.png';
+import emotion from '../../Assets/Projects/emotion.png';
+import editor from '../../Assets/Projects/codeEditor.png';
+import chatify from '../../Assets/Projects/chatify.png';
+import suicide from '../../Assets/Projects/suicide.png';
+import bitsOfCode from '../../Assets/Projects/blog.png';
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: 'white' }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: 'center', paddingBottom: '10px' }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatify}
+              isBlog={false}
+              title="DevRev-AirLine"
+              description="DevRev-Air, a highly efficient and reliable backend application responsible for seamlessly managing multiple facets of flight operations, including flight management, booking systems, real-time notifications, and secure payment processing. It incorporates a Microservice MVC architecture."
+              ghLink="https://github.com/iamRishabh07/DevRev-AirLine"
+              //demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={bitsOfCode}
+              isBlog={false}
+              title="HotelBookingApp"
+              description="HotelBookingApp is a comprehensive web application designed to streamline task management and hotel booking processes for users. The application offers a user-friendly interface for managing to-do lists, searching for hotels, and processing bookings, enhancing the overall user experience with efficient data fetching and seamless payment transactions."
+              ghLink="https://github.com/iamRishabh07/HotelBookingApp"
+              //demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={editor}
+              isBlog={false}
+              title="Invoice Generator"
+              description="An Invoice creator project built with React. Add itemized items, configure quantity, prices, tax rates and discounts. Download Invoice as PDFs to your device. Uses jspdf-react to capture the data from the modal and covert it from canvas -> pdf."
+              ghLink="https://github.com/iamRishabh07/invoice-generator"
+              //demoLink="https://editor.soumya-jit.tech/"
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Plant AI"
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
